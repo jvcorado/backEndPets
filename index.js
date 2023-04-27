565062eb3b02e87217b9855223480dd602ab5346
João Victor Corado <jhonisilva545@gmail.com>
2023-04-27 16:22:42 -0300

    teste

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.post("/blog", upload.single("image"), async (req, res) => {
   
     let info = await transporter.sendMail({
       from: "contatopets@petsmellon.com.br",
-      to: "benolopesdias@gmail.com",
+      to: "jenifer.alves@patense.com.br",
       subject: "Contato pelo Site",
       html: `
         <p><strong>Nome:</strong> ${nome}</p>
